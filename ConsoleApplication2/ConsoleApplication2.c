6fb7534c7b52116ffb386d85bedeaa0af1cf3452
Hartov Stanislav <s.xartov@mail.ru>
2020-10-13 14:19:29 +0300

    add base

--- ConsoleApplication2/ConsoleApplication2.c
+++ ConsoleApplication2/ConsoleApplication2.c
@@ -1,3 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct ListElement
+{
+	int value;
+	struct ListElement* next;
+} ListElement;
+
+typedef struct ListHeadTail
+{
+	struct ListHeadTail* head;
+	struct ListHeadTail* tail;
+} ListHeadTail;
+
 void main() {
-	43243232
+
 }
